2f929b91223a9f7c3d2545aee08ece8d8fe153ca
danielTM999 <danielmelo101203@gmail.com>
2023-10-11 06:43:22 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+    require "./vendor/autoload.php";
+
+    use Rest_Router\Router;
+
+    $app = new Router();
+
+
+
+?>
---
+++ src/Routes.php
@@ -0,0 +1,63 @@
+<?php
+
+    namespace Rest_Router;
+
+    class Router{
+        private $routes = [];
+
+        public function get($path, $callback)
+        {
+            $this->routes[] = ['method' => 'GET', 'path' => $path, 'callback' => $callback];
+        }
+
+        public function post($path, $callback)
+        {
+            $this->routes[] = ['method' => 'POST', 'path' => $path, 'callback' => $callback];
+        }
+
+        public function run(){
+            $requestPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+            $requestMethod = $_SERVER['REQUEST_METHOD'];
+            $requestPathexpected = $requestPath;
+
+            foreach ($this->routes as $route) {
+                $padrao = "/\{([^}]+)\}/";
+                $varId = null;
+                $indexOf = 0;
+                if(preg_match($padrao, $route['path'], $correspondencias)){
+                    $varId = $correspondencias[1];
+                    $indexOf = strpos($route['path'], "{");
+                    $route["path"] = substr($route['path'], 0, $indexOf);
+                    $requestPathexpected = substr($requestPath,0, $indexOf);
+
+                }
+                if ($route['method'] == $requestMethod && $route['path'] == $requestPathexpected) {
+
+                    if(isset($varId)){
+                        $server = [
+                            "request" => [
+                                "body" => file_get_contents("php://input"),
+                                "header" => getallheaders()
+                            ],
+                            "$varId" => substr($requestPath, $indexOf, strlen($requestPath))
+                        ];
+                    }else{
+                        $server = [
+                            "request" => [
+                                "body" => file_get_contents("php://input"),
+                                "header" => getallheaders()
+                            ],
+                        ];
+                    }
+
+                    call_user_func($route['callback'], $server);
+                    return;
+                }
+                http_response_code(404);
+                echo "Página não encontrada.";
+            }
+
+        }
+    }
+
+?>
